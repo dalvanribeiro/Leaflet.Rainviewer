6c26b6708e66df2d5d2c072692e8b00bb94a4381
Dalvan Ribeiro de Almeida <dalvan.almeida@ufes.br>
2025-01-29 14:29:19 -0300

    teste 3

--- leaflet.rainviewer.js
+++ leaflet.rainviewer.js
@@ -32,7 +32,7 @@ L.Control.Rainviewer = L.Control.extend({
 
         this.link = L.DomUtil.create('a', 'leaflet-control-rainviewer-button leaflet-bar-part', this.container);
         this.link.href = '#';
-        this.load(map);
+        this.load(this);
         L.DomEvent.on(this.link, 'click', this.load, this);
         return this.container;
 
